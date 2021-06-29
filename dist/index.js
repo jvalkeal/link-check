a98bdb71f7d435a09ef782f5a27278e76695f610
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-06-29 09:37:57 +0100

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -8187,7 +8187,7 @@ const core = __importStar(__webpack_require__(470));
 const links_handler_1 = __webpack_require__(733);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        const fail = Boolean(inputNotRequired('config'));
+        const fail = inputNotRequired('fail') === 'false' ? false : true;
         // default output when links handling fails
         let results = {
             status: false,
